238a5bec95f1bb8ded22d3c4752b06268f004983
Mohzeela <wahley93@gmail.com>
2024-02-26 15:24:57 +0100

    updated helm provider

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "aks_named_id" {
-  value = module.aks.aks_id
-}
+# output "aks_named_id" {
+#   value = module.aks.aks_id
+# }
+
+# output "kube_config" {
+#   value = module.aks.kube_config_raw
+
+#   sensitive = true
+# }
--- providers.tf
+++ providers.tf
@@ -35,23 +35,25 @@ provider "azurerm" {
   subscription_id = "32eb20d3-efa4-4a51-a2ff-32721933e3dd"
 }
 
-provider "random" {}
+# provider "random" {}
 
-# provider "helm" {
-#   kubernetes {
-#     host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
-#     client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
-#     client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)
-#     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host                   = module.aks.host
+    client_certificate     = base64decode(module.aks.client_certificate)
+    client_key             = base64decode(module.aks.client_key)
+    cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
+  }
+}
+
 
 # data "aws_aks_cluster_auth" "cluster" {
 #   name = module.aks_cluster_name.aks_id
 # }
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     # config_path = "~/.kube/config"
+#     host = module.aks.admin_host
+#   }
+# }
